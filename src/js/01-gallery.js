4fe56cb55d8e79bf4dfeafb35fcdaf7f260f0e04
Iryna Karpusenko <karpusenko.irina.13@gmail.com>
2023-04-07 07:27:30 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,12 +1,9 @@
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
-
 const galleryContainer=document.querySelector('.gallery');
 
 
@@ -14,16 +11,11 @@ function createGalleryItemsList(galleryItems) {
     return galleryItems.map(item => {
       const { preview, original, description } = item;
       return `
-        <li class="gallery__item">
-          <a class="gallery__link" href="${original}">
-            <img
-              class="gallery__image"
-              src="${preview}"
-              data-source="${original}"
-              data-alt="${description}"
-            />
-          </a>
-        </li>
+      <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+   </li>
       `;
     }).join('');
   }
@@ -33,38 +25,14 @@ function createGalleryItemsList(galleryItems) {
 galleryContainer.insertAdjacentHTML('beforeend', galleryMarkUp);
 
 
-function onGalleryContainerClick(event) {
-  event.preventDefault();
 
-  const {dataset:{source}}=event.target;
-if (!source) {return;
-}
-const instance= basicLightbox.create(`
-    <div class="modal">
-      <img src="${source}", width="640px" />
-      <a>Close</a> </div>`, {
-    onShow: (instance) =>{
-      const onKeyDown=(event) => {
-        if (event.key=== 'Escape') {
-          instance.close();
-        }
-      };
-      document.addEventListener('keydown', onKeyDown);
-      instance.element().querySelector('a').onclick = instance.close;
-    },
-    onClose: (instance) => {
-      const onKeyDown= (event) => {
-        if (event.key=== 'Escape') {
-          instance.close();
-        }
-      };
-      document.removeEventListener('keydown', onKeyDown);
-    }
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+    captionsData:'alt'
   });
 
-  instance.show();
-}
 
-galleryContainer.addEventListener('click', onGalleryContainerClick);
 
 
