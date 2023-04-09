801d07e134c8f0815a4f4cb3ef30a134f79b4b1e
Iryna Karpusenko <karpusenko.irina.13@gmail.com>
2023-04-09 07:52:30 +0300

    дз 8, задачі номер 2, 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+// // Додай бібліотеку як залежність проекту через npm.
+import VimeoPlayer from '@vimeo/player';
+// // Додай до проекту бібліотеку lodash.throttle
+import throttle from 'lodash.throttle';
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+// // Ініціалізуй плеєр у файлі скрипта як це описано в
+// //  секції pre - existing player, але враховуй, що у тебе плеєр доданий як npm пакет, а не через CDN.
+const iframe = document.querySelector('iframe');
+    const player = new VimeoPlayer(iframe);
+
+player.on('timeupdate', throttle((data) => {
+  localStorage.setItem(LOCALSTORAGE_KEY , data.seconds);
+}, 1000));
+
+player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+player.ready().then(() => {
+  const currentTime = localStorage.getItem(LOCALSTORAGE_KEY );
+  if (currentTime) {
+    player.setCurrentTime(currentTime);
+  }
+});
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,76 @@
+import throttle from 'lodash.throttle';
 
+import "../css/common.css";
+import "../css/03-feedback.css"
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+const form = document.querySelector(".js-feedback-form");
+const textarea = document.querySelector("textarea");
+const input = document.querySelector("input");
+
+const formData = {
+  email: '',
+  message: ''
+};
+
+populateTextareaEmail();
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', onFormInput);
+textarea.addEventListener('input', throttle(onTextareaInput, 500));
+input.addEventListener('input', throttle(onInput, 500));
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    console.log("Please fill in all fields");
+    return;
+  }
+
+  console.log("send form");
+  event.target.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  console.log(formData);
+}
+
+function onTextareaInput(event) {
+  const message = event.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify({ ...formData, message }));
+  console.log(message);
+}
+
+function onInput(event) {
+  const email = event.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify({ ...formData, email }));
+  console.log(email);
+}
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+}
+
+function populateTextareaEmail() {
+  const savedState = localStorage.getItem(LOCALSTORAGE_KEY);
+
+  if (savedState) {
+    try {
+      const { email, message } = JSON.parse(savedState);
+
+      if (email) {
+        input.value = email;
+        formData.email = email;
+      }
+
+      if (message) {
+        textarea.value = message;
+        formData.message = message;
+      }
+
+    } catch (error) {
+      console.error(error);
+      // handle error
+    }
+  }
+}
